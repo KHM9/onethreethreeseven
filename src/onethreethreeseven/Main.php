0e943958a929fc98e2f340200555bf63429edbfe
KHM9 <45921659+KHM9@users.noreply.github.com>
2020-05-04 22:07:46 -0600

    Update Main.php

--- src/onethreethreeseven/Main.php
+++ src/onethreethreeseven/Main.php
@@ -8,26 +8,24 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-
-class main extends PluginBase {
     
-    public function onLoad(): void{
-        $this->getLogger()->info(C::GOLD . "PLugin is almost done loading!");
-    }
-    public function onEnable(): void{
-        $this->getLogger()->info(C::GREEN . "Plugin is now enabled! :D");
-    }
-    public function onDisable(): void{
-        $this->getLogger()->info(C::RED . "ERROR: Plugin has been disabled :( Contact KHM9 for more info...");
-    }
-    public function onCommand(CommandSender $sender, Command $cmd, String $Label, array $args): bool{
-        if($cmd->getName()){
-            switch($args(count) === 0){
-              case "example":
-                $player->sendMessage(C::GREEN . "ONETHREETHREESEVEN - DEVELOPED BY KHM9 - IN DEVELOPMENT");
-                break;
-            }
-        return true;
-        }
-    }
+class Main extends PluginBase implements Listener{
+
+	public function onEnable(){
+		$this->getLogger()->info("Hello World!");
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()){
+			case "example":
+				$sender->sendMessage("Example command output");
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public function onDisable(){
+		$this->getLogger()->info("Bye!");
+	}
 }
